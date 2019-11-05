660cd991116fd56ab3d467a32956d9cc5752a4c1
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-05 17:26:59 +0200

    Create lesson2_hw03.py

---
+++ lesson2_hw03.py
@@ -0,0 +1,10 @@
+import random
+a = random.randint (1,1000)
+print ("Число a =" ,a)
+import math
+print ("Квадрат числа а = ",math.sqrt (a))
+answer1 = a > 10
+print ("Число а больше 10?",answer1)
+b = math.sqrt (a)
+answer2 = b > 500
+print ("Квадрат числа a больше 500?",answer2)
