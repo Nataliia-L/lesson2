57b0260708bab905fa071af724ea3a9a529464eb
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-09 20:55:47 +0200

    Update lesson2_hw02.py

--- lesson2_hw02.py
+++ lesson2_hw02.py
@@ -5,5 +5,5 @@ print ("Результат 1: " ,c,".", "Тип:", type (c))
 import math
 d = math.pow(a,b)
 print ("Результат 2: ",d,".", "Тип:", type (d))
-print ("Значение c больше значения d?")
-print (c>d)
+print ("Значение c равно значению d?")
+print (c==d)
