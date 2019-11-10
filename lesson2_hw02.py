d6142a07fccc15162385f6de6f60371d0ead5d34
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-10 13:16:24 +0200

    Update lesson2_hw02.py

--- lesson2_hw02.py
+++ lesson2_hw02.py
@@ -1,9 +1,9 @@
 a = int (input ("Введите число  "))
 b = int (input ("Укажите степень числа  "))
-c = a ** b
-print ("Результат 1: " ,c,".", "Тип:", type (c))
+val_1 = a ** b
+print ("Результат 1: " ,val_1,".", "Тип:", type (val_1))
 import math
-d = math.pow(a,b)
-print ("Результат 2: ",d,".", "Тип:", type (d))
-print ("Значение c равно значению d?")
-print (c==d)
+val_2 = math.pow(a,b)
+print ("Результат 2: ",val_2,".", "Тип:", type (val_2))
+print ("Значение val_1 равно значению val_2?")
+print (val_1==val_2)
