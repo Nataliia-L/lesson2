d601a6b3f0def3219af96c4f9ea4833d0766b524
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-10 13:17:58 +0200

    Update lesson2_hw03.py

--- lesson2_hw03.py
+++ lesson2_hw03.py
@@ -1,10 +1,10 @@
 import random
-val1 = random.randint (1,1000)
-print ("Число val_1 =" ,val1)
+val_1 = random.randint (1,1000)
+print ("Число val_1 =" ,val_1)
 import math
-print ("Квадрат числа val_1 = ",math.sqrt (val1))
-answer1 = val1 > 10
+print ("Квадрат числа val_1 = ",math.sqrt (val_1))
+answer1 = val_1 > 10
 print ("Число val_1 больше 10?",answer1)
-val2 = math.sqrt (val1)
-answer2 = val2 > 500
+val_2 = math.sqrt (val_1)
+answer2 = val_2 > 500
 print ("Квадрат числа val_1 больше 500?",answer2)
